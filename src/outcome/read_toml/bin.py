db7c5c982f30ed9c9ccd33ed6cbbbd104e8adf5b
AdrienArtaud <adrien.artaud@outcome.co>
2020-08-03 17:47:25 +0200

    feat: add check-only flag

--- src/outcome/read_toml/bin.py
+++ src/outcome/read_toml/bin.py
@@ -12,7 +12,8 @@ from outcome.read_toml.lib import read  # noqa: WPS347
 @click.command()
 @click.option('--path', help='The path to the TOML file', required=True, type=click.File('r'))
 @click.option('--key', help='The path to read from the TOML file', required=True)
-def read_toml(path, key: str):
+@click.option('--check-only', help='If present, only checks if the key is present in the TOML file', is_flag=True, default=False)
+def read_toml(path, key: str, check_only: bool):
     """Read the value specified by the path from a TOML file.
 
     The path parameter should be a '.' separated sequences of keys
@@ -44,18 +45,30 @@ def read_toml(path, key: str):
 
     read_toml.py --path my_file.toml --key tools -> #ERROR
 
+    Check if key exists:
+
+    read_toml.py --path my_file.toml --key tools --check-only -> 0 if key exists
+    read_toml.py --path my_file.toml --key tools --check-only -> 1 if key does not exists
+
     Args:
         path (str): The path to the file.
         key (str): The path to the key to read.
+        check_only (bool): If True, only checks if key exists
+
     """
     try:
-        output(key, read(path, key))
+        output(key, read(path, key), check_only=check_only)
     except KeyError as ex:
-        fail(str(ex))
+        if check_only:
+            say(1)
+        else:
+            fail(str(ex))
 
 
-def output(key: str, value: str):
-    if os.environ.get('GITHUB_ACTIONS', False):
+def output(key: str, value: str, check_only: bool = False):
+    if check_only:
+        say(0)
+    elif os.environ.get('GITHUB_ACTIONS', False):
         action_key = key.replace('.', '_')
         say(f'::set-output name={action_key}::{value}')
     else:
--- test/test_bin.py
+++ test/test_bin.py
@@ -40,6 +40,11 @@ class TestOutput:
         read_toml.output(output_case['key'], output_case['value'])
         mock_say.assert_called_once_with(output_case['value'])
 
+    @patch.dict('os.environ', {}, clear=True)
+    def test_check_only_without_github(self, mock_say: Mock, output_case):
+        read_toml.output(output_case['key'], output_case['value'], check_only=True)
+        mock_say.assert_called_once_with(0)
+
 
 @pytest.fixture
 def isolated_filesystem_runner(sample_toml):
@@ -59,7 +64,17 @@ class TestCommand:
         mock_read.return_value = '123'
         result = isolated_filesystem_runner.invoke(read_toml.read_toml, ['--path', './sample.toml', '--key', 'my_key'])
         assert result.exit_code == 0
-        mock_output.assert_called_once_with('my_key', '123')
+        mock_output.assert_called_once_with('my_key', '123', check_only=False)
+
+    @patch('outcome.read_toml.bin.read', autospec=True)
+    @patch('outcome.read_toml.bin.output', autospec=True)
+    def test_call_only_check(self, mock_output: Mock, mock_read: Mock, isolated_filesystem_runner):
+        mock_read.return_value = '123'
+        result = isolated_filesystem_runner.invoke(
+            read_toml.read_toml, ['--path', './sample.toml', '--key', 'my_key', '--check-only'],
+        )
+        assert result.exit_code == 0
+        mock_output.assert_called_once_with('my_key', '123', check_only=True)
 
     @patch('outcome.read_toml.bin.read', autospec=True)
     @patch('outcome.read_toml.bin.output', autospec=True)
@@ -67,3 +82,13 @@ class TestCommand:
         mock_read.side_effect = KeyError('my_key')
         result = isolated_filesystem_runner.invoke(read_toml.read_toml, ['--path', './sample.toml', '--key', 'my_key'])
         assert result.exit_code != 0
+
+    @patch('outcome.read_toml.bin.read', autospec=True)
+    @patch('outcome.read_toml.bin.say', autospec=True)
+    def test_call_failure_only_check(self, mock_say: Mock, mock_read: Mock, isolated_filesystem_runner):
+        mock_read.side_effect = KeyError('my_key')
+        result = isolated_filesystem_runner.invoke(
+            read_toml.read_toml, ['--path', './sample.toml', '--key', 'my_key', '--check-only'],
+        )
+        assert result.exit_code == 0
+        mock_say.assert_called_once_with(1)
