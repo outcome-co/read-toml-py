e91ba168a19ab7d86e454aa299a7764c5e2d80e0
Douglas Willcocks <douglas@d-tw.org>
2020-10-20 14:25:32 +0200

    fix: path types (#29)
    * fix: handle multiple path types
    
    * style: fix linting

--- src/outcome/read_toml/bin.py
+++ src/outcome/read_toml/bin.py
@@ -3,7 +3,8 @@
 """A utility to read values from TOML files."""
 
 import sys
-from typing import IO, Optional
+from pathlib import Path
+from typing import IO, Optional, Union
 
 import click
 from outcome.read_toml.lib import read  # noqa: WPS347
@@ -64,10 +65,19 @@ def read_toml_cli(path: IO[str], key: str, check_only: bool, github_actions: boo
 
 
 def read_toml(
-    path: IO[str], key: str, check_only: bool = False, github_actions: bool = False, default: Optional[str] = None,
+    path: Union[IO[str], str, Path],
+    key: str,
+    check_only: bool = False,
+    github_actions: bool = False,
+    default: Optional[str] = None,
 ):  # noqa: WPS216
     try:
-        value = read(path, key)
+        if isinstance(path, (str, Path)):
+            with open(path, 'r') as path_handle:
+                value = read(path_handle, key)
+        else:
+            value = read(path, key)
+
         if check_only:
             value = '1'
     except KeyError as ex:
